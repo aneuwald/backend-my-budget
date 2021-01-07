d224ebdb33a1c41f484ea71453293ad28961cd04
aneuwald <aneuwald@gmail.com>
2021-01-07 22:37:12 +0000

    era body mesmo

--- src/api/temp.ts
+++ src/api/temp.ts
@@ -8,12 +8,7 @@ const GET_TEMPS = async (req: Request, res: Response): Promise<Response> => {
 }
 
 const INSERT_TEMP = async (req: Request, res: Response): Promise<Response> => {
-  const { temp, umid } = req.query
-
-  console.log('body', req.body)
-  console.log('query', req.query)
-  console.log('params', req.params)
-
+  const { temp, umid } = req.body
   return Temp.create({ temp, umid })
     .then(c => res.send(c))
     .catch(() => res.send('Algo deu errado ao criar!'))
